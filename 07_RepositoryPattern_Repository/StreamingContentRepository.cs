42e2197cc6d30eb67917d492eb6914661414d6b6
kyldnewt99 <newky99@gmail.com>
2020-09-16 14:50:33 -0400

    after lunch

--- 07_RepositoryPattern_Repository/StreamingContentRepository.cs
+++ 07_RepositoryPattern_Repository/StreamingContentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace _07_RepositoryPattern_Repository
         protected readonly List<StreamingContent> _contentDirectory = new List<StreamingContent>();
         //CRUD - Create Read Update Delete
 
-
+        //CREATE
         public bool AddContentToDirectory(StreamingContent content)
         {
             int startingCount = _contentDirectory.Count;
@@ -19,11 +20,51 @@ namespace _07_RepositoryPattern_Repository
             bool wasAdded = (_contentDirectory.Count > startingCount) ? true : false;
             return wasAdded;
         }
-        //READ
+        //READ ALL
         public List<StreamingContent> GetContents()
         {
             return _contentDirectory;
         }
+        //READ ONE
+        public StreamingContent GetContentByTitle(string title)
+        {
+            foreach(StreamingContent singleContent in _contentDirectory)
+            {
+                if(singleContent.Title.ToLower()==title.ToLower())
+                {
+                    return singleContent;
+                }
+            }
+            return null;
+        }
 
+        //Update
+        public bool UpdateExistingContent(string originalTitle, StreamingContent newContent)
+        {
+            StreamingContent oldContent = GetContentByTitle(originalTitle);
+
+            if (oldContent != null)
+            {
+                oldContent.Title = newContent.Title;
+                oldContent.Description = newContent.Description;
+                oldContent.StarRating = newContent.StarRating;
+                oldContent.MaturityRating = newContent.MaturityRating;
+                oldContent.IsFamilyFriendly = newContent.IsFamilyFriendly;
+                oldContent.TypeOfGenre = newContent.TypeOfGenre;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        //Delete
+        public bool DeleteExistingContent(StreamingContent existingContent)
+        {
+            bool deleteResult = _contentDirectory.Remove(existingContent);
+            return deleteResult;
+        }
     }
 }
--- 07_RepositoryPattern_Tests/StreamingContentRepositoryTests.cs
+++ 07_RepositoryPattern_Tests/StreamingContentRepositoryTests.cs
@@ -42,5 +42,47 @@ namespace _07_RepositoryPattern_Tests
             Assert.IsTrue(directoryHasContent);
 
         }
+
+        private StreamingContentRepository _repo;
+        private StreamingContent _content;
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new StreamingContentRepository();
+            _content = new StreamingContent("Oceans 8", "do crime?", 100, "I do not know", true, GenreType.Action);
+            _repo.AddContentToDirectory(_content);
+        }
+        [TestMethod]
+        public void GetByTitle_ShouldReturnCorrectContent()
+        {
+            //Arrange
+            //Act
+            StreamingContent searchResult = _repo.GetContentByTitle("Oceans 8");
+
+            //Assert
+            Assert.AreEqual(_content, searchResult);
+        }
+        [TestMethod]
+        public void UpdateExistingContent_ShouldReturnTrue()
+        {
+            //arrange
+            StreamingContent updatedContent = new StreamingContent("Italian Job", "do crime? in Italy", 100, "I do not know", true, GenreType.Action);
+
+            //Act
+            bool updateResult = _repo.UpdateExistingContent("Oceans 8", updatedContent);
+
+            //Assert
+            Assert.IsTrue(updateResult);
+        }
+
+        public void DeleteExistingContent_ShouldReturnTrue()
+        {
+            //arrange
+            StreamingContent foundContent = _repo.GetContentByTitle("Oceans 8");
+            //Act
+            bool removeResult = _repo.DeleteExistingContent(foundContent);
+            //Assert
+            Assert.IsTrue(removeResult);
+        }
     }
 }
