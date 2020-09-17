172d8211232045d9644efc61f9bddd4d97ca002a
kyldnewt99 <newky99@gmail.com>
2020-09-17 11:45:03 -0400

    thurs morning

---
+++ 07_RepositoryPattern_Repository/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_RepositoryPattern_Repository
+{
+    public class Movie : StreamingContent
+    {
+        public double RunTime { get; set; }
+        public Movie() {}
+
+        public Movie(string title, string description, float starRating, string mRating, bool isFamilyFriendly, GenreType tOG, double runTime)
+        :base(title, description, starRating,mRating,isFamilyFriendly,tOG)
+        {
+            RunTime = runTime;
+        }
+    }
+}
